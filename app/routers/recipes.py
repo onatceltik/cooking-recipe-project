0047d9fb0681c5cd414e4521e145fc9617551278
Onat Çeltik <onat.celtik27@gmail.com>
2026-04-17 13:15:28 +0300

    added: rest of the recipe main CRUD functions

--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -21,10 +21,11 @@ def get_db():
     try:
         yield db
     finally:
+        # Runs after the calling function returns
         db.close()
 
 
-# CREATE A RECIPE
+# CREATE A RECIPE (POST /recipes/)
 # Tell FastAPI to format the output using Response Schema
 @router.post("/", response_model=recipe_schemas.Recipe)
 def create_recipe(recipe: recipe_schemas.RecipeCreate, db: Session = Depends(get_db)):
@@ -40,9 +41,62 @@ def create_recipe(recipe: recipe_schemas.RecipeCreate, db: Session = Depends(get
     # FastAPI will automatically convert this database model back into the Pydantic Response schema
     return db_recipe
 
-# READ ALL RECIPES
+# READ ALL RECIPES (GET /recipes/)
 @router.get("/", response_model=List[recipe_schemas.Recipe])
 def get_recipes(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
     # Query the database, skip a certain amount (for pagination), and limit the results
     recipes = db.query(recipe_models.Recipe).offset(skip).limit(limit).all()
     return recipes
+
+# READ ONE RECIPE (GET /recipes/{id})
+@router.get("/{recipe_id}", response_model=recipe_schemas.Recipe)
+def get_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    db_recipe = db.query(recipe_models.Recipe).filter(recipe_models.Recipe.id == recipe_id).first()
+
+    # If recipe doesn't exists, return 404 Not Found error
+    if db_recipe == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Recipe not found!"
+        )
+    return db_recipe
+
+# UPDATE A RECIPE (PUT /recipes/{id})
+@router.put("/{recipe_id}", response_model=recipe_schemas.Recipe)
+def update_recipe(recipe_id: int, updated_recipe: recipe_schemas.RecipeCreate, db: Session = Depends(get_db)):
+    db_recipe = db.query(recipe_models.Recipe).filter(recipe_models.Recipe.id == recipe_id).first()
+
+    # If recipe doesn't exists, return 404 Not Found error
+    if db_recipe == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Recipe not found!"
+        )
+    
+    # Update the values
+    updated_data = updated_recipe.model_dump()
+    for key,value in updated_data.items():
+        setattr(db_recipe, key, value)
+
+    # Save the change
+    db.commit()
+    db.refresh(db_recipe)
+
+    return db_recipe
+
+# DELETE A RECIPE (DELETE /recipes/{id})
+@router.delete("/{recipe_id}")
+def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    db_recipe = db.query(recipe_models.Recipe).filter(recipe_models.Recipe.id == recipe_id).first()
+
+    # If recipe doesn't exists, return 404 Not Found error
+    if db_recipe == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Recipe not found!"
+        )
+    
+    db.delete(db_recipe)
+    db.commit()
+
+    return {"detail": "Recipe is successfully deleted!"}
